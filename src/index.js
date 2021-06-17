18b6c3e63ffb4c3cd987920d77f6eaf80e06782f
Sahil Memon <sahilm1899@gmail.com>
2021-06-17 20:11:10 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,53 +1,49 @@
+require("./models/User");
+require("./models/Track");
+const mongoURI = require("./mongoDB");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
+const trackRoutes = require("./routes/trackRoutes");
+const requireAuth = require("./middlewares/requireAuth");
+
 /*
-Not assigning the User model & Track model
-to any variable since that file needs to run
-just once., i.e. the models needs to be
-decrared just once.
+Not assigning the User model & Track model to variables since they
+need to run just once when the models are declared initially.
 */
 
-require('./models/User');
-require('./models/Track');
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const authRoutes = require('./routes/authRoutes');
-const trackRoutes = require('./routes/trackRoutes');
-const requireAuth = require('./middlewares/requireAuth');
-
-//Express API.
+// express used for exposing the API.
 const app = express();
 
-//Used for parsing the request body.
+// Parse the request body.
 app.use(bodyParser.json());
 
-//Associating the respective route handler with this app.
+// Link the route handlers.
 app.use(authRoutes);
 app.use(trackRoutes);
 
-const mongoURI = 'mongodb+srv://admin:passwordpassword@cluster0-jmyxk.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority'
-
-//Mongoose helps connect to the mongoDB instance.
+// Mongoose helps connect to the mongoDB instance.
 mongoose.connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
-mongoose.connection.on('connected', () => {
-    console.log('Connected to mongo instance!');
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongoDB instance!");
 });
 
-mongoose.connection.on('erorr', (err) => {
-    console.log('Error connecting to mongo', err);
+mongoose.connection.on("erorr", (err) => {
+  console.log("Error connecting to mongoDB", err);
 });
 
-//Making an HTTP request. req - request, res - response.
-//Passing the jwt middlware to the initial request.
-app.get('/', requireAuth, (req, res) => {
-    res.send(`Your email: ${req.user.email}`);
+// Pass the jwt middlware to the initial request.
+app.get("/", requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
 });
 
 // Listening port set to 3000.
 app.listen(3000, () => {
-    console.log('Listening on port 3000');
+  console.log("Listening on port 3000");
 });
