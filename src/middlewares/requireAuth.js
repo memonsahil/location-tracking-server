2d84ed7a0b7a4d6804d86ef625e28af218f5b3bb
Sahil Memon <sahilm1899@gmail.com>
2021-06-18 08:31:55 +0100

    Update requireAuth.js

--- src/middlewares/requireAuth.js
+++ src/middlewares/requireAuth.js
@@ -1,29 +1,27 @@
-const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
-const User = mongoose.model('User');
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const User = mongoose.model("User");
 
-//jwt middleware for verifying the authentication.
 requireAuth = (req, res, next) => {
-    const { authorization } = req.headers;
-    //authorization = "Bearer token_created"
+  const { authorization } = req.headers;
 
-    if (!authorization) {
-        return res.status(401).send({ error: 'You must be logged in.' });
+  if (!authorization) {
+    return res.status(401).send({ error: "You must be logged in." });
+  }
+
+  const token = authorization.replace("Bearer ", "");
+
+  jwt.verify(token, "MY_SECRET_KEY", async (err, payload) => {
+    if (err) {
+      return res.status(401).send({ error: "You must be logged in." });
     }
 
-    const token = authorization.replace('Bearer ', '');
-    
-    jwt.verify(token, 'MY_SECRET_KEY', async (err, payload) => {
-        if (err) {
-            return res.status(401).send({ error: 'You must be logged in.' });
-        }
+    const { userId } = payload;
 
-        const { userId } = payload;
-
-        const user = await User.findById( userId );
-        req.user = user; //Assign that user instance to req for any other request handlers further.
-        next()  //Go to the next middleware or proceed.
-    });
+    const user = await User.findById(userId);
+    req.user = user;
+    next();
+  });
 };
 
 module.exports = requireAuth;
