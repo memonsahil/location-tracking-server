ad3742f2fd6af3c08f286ac3774dd2b191699761
Sahil Memon <sahilm1899@gmail.com>
2021-06-18 08:30:31 +0100

    Update routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,71 +1,47 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
-const User = mongoose.model('User');    //Importing the User model.
+const express = require("express");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const User = mongoose.model("User");
 
-//Route handler for authentication requests.
 const router = express.Router();
 
-//Signup request
-router.post('/signup', async (req, res) => {
-    const { email, password } = req.body;
+router.post("/signup", async (req, res) => {
+  const { email, password } = req.body;
 
-    /*
-    Creating a new user instance by 
-    passing in the email and password
-    to the User model.
-    */
+  if (!email || !password) {
+    return res.status(422).send({ error: "Invalid password or email" });
+  }
 
-    try {
-        
+  try {
     const user = new User({ email, password });
     await user.save();
-    //Save the user through an asynchronous operation.
 
-    /*
-    jwt creates a token that is used for
-    authentication and for any follow-up requests.
-    */
-   
-    const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
+    const token = jwt.sign({ userId: user._id }, "MY_SECRET_KEY"); // Add jwt
     res.send({ token: token });
-
-    } catch (err) {
-
-    /*
-    Code 422 means that the data (email, password)
-    passed to create a new user appears to be invalid,
-    i.e. a user with that data already exists or no data
-    is provided. Also send the relavant error message.
-    */
-
+  } catch (err) {
     return res.status(422).send(err.message);
-
-    }
+  }
 });
 
-//Signin request
-router.post('/Signin', async (req, res) => {
-    const { email, password } = req.body;
+router.post("/Signin", async (req, res) => {
+  const { email, password } = req.body;
 
-    //Opaque error messages for secracy.
-    if (!email || !password) {
-        return res.status(422).send({ error: 'Invalid password or email' });
-    }
+  if (!email || !password) {
+    return res.status(422).send({ error: "Invalid password or email" });
+  }
 
-    const user = await User.findOne({ email: email });
-    if (!user) {
-        return res.status(422).send({ error: 'Invalid password or email'});
-    }
+  const user = await User.findOne({ email: email });
+  if (!user) {
+    return res.status(422).send({ error: "Invalid password or email" });
+  }
 
-    try {
-        await user.comparePassword(password);
-        const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY'); //Updating the token.
-        res.send ({ token: token });
-    } catch (err) {
-        return res.status(422).send({ error: 'Invalid password or email' });
-    }
- 
+  try {
+    await user.comparePassword(password);
+    const token = jwt.sign({ userId: user._id }, "MY_SECRET_KEY"); // Update jwt
+    res.send({ token: token });
+  } catch (err) {
+    return res.status(422).send({ error: "Invalid password or email" });
+  }
 });
 
 module.exports = router;
--- src/routes/trackRoutes.js
+++ src/routes/trackRoutes.js
@@ -1,38 +1,37 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const requireAuth = require('../middlewares/requireAuth');
-const Track = mongoose.model('Track');
+const express = require("express");
+const mongoose = require("mongoose");
+const requireAuth = require("../middlewares/requireAuth");
+const Track = mongoose.model("Track");
 
 const router = express.Router();
 
-router.use(requireAuth); //Requests made using this router require the user to be signed in.
+// Requests made using this router require the user to be signed in.
+router.use(requireAuth);
 
-//Tracks request
-router.get('/tracks', async (req, res) => {
-    console.log(req);
-    const tracks = await Track.find({ userId: req.user._id });
-    
-    res.send(tracks);
+router.get("/tracks", async (req, res) => {
+  console.log(req);
+  const tracks = await Track.find({ userId: req.user._id });
+
+  res.send(tracks);
 });
 
-//Create Track request
-router.post('/tracks', async (req, res) => {
-    const { name, locations } = req.body;
+router.post("/tracks", async (req, res) => {
+  const { name, locations } = req.body;
 
-    if (!name || !locations) {
-        return res.status(422).send({ error: 'You must provide a name and locations.' });
-    }
+  if (!name || !locations) {
+    return res
+      .status(422)
+      .send({ error: "You must provide a name and a set of locations." });
+  }
 
-    try {
-        const track = new Track({ name, locations, userId: req.user._id });
-        await track.save();
+  try {
+    const track = new Track({ name, locations, userId: req.user._id });
+    await track.save();
 
-        res.send(track);
-
-    } catch(err) {
-        res.status(422).send({error: err.message});
-    }
-
+    res.send(track);
+  } catch (err) {
+    res.status(422).send({ error: err.message });
+  }
 });
 
 module.exports = router;
