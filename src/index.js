dc436be76495de8e0b791d83022d644705ef5500
Sahil Memon <sahilm1899@gmail.com>
2021-06-18 08:49:27 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,27 +3,19 @@ require("./models/Track");
 const mongoURI = require("./mongoDB");
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
 const trackRoutes = require("./routes/trackRoutes");
 const requireAuth = require("./middlewares/requireAuth");
 
-/*
-Not assigning the User model & Track model to variables since they
-need to run just once when the models are declared initially.
-*/
-
 // express used for exposing the API.
 const app = express();
 
-// Parse the request body.
-app.use(bodyParser.json());
+app.use(express.json());
 
 // Link the route handlers.
 app.use(authRoutes);
 app.use(trackRoutes);
 
-// Mongoose helps connect to the mongoDB instance.
 mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -38,12 +30,10 @@ mongoose.connection.on("erorr", (err) => {
   console.log("Error connecting to mongoDB", err);
 });
 
-// Pass the jwt middlware to the initial request.
 app.get("/", requireAuth, (req, res) => {
   res.send(`Your email: ${req.user.email}`);
 });
 
-// Listening port set to 3000.
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
